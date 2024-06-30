bd25db91ea54741095b5131789c85949ade1db47
Jayapratha Palanisamy <jayaprathainbox1@gmail.com>
2024-06-30 23:52:50 +0100

    Changes

---
+++ src/test/java/com/jaya/parkinglot/ParkinglotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaya.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
